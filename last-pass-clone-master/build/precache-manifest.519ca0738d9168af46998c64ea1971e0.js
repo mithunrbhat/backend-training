ba7239d4bcc1f605c49ad2e906eaca82e76a1f2e
mithunrbhat <75619252+mithunrbhat@users.noreply.github.com>
2021-04-06 10:29:35 +0530

    last-pass-src

---
+++ last-pass-clone-master/build/precache-manifest.519ca0738d9168af46998c64ea1971e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7813ebaaa7cfe6b1d2f64ae300b21d14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3c34370f12d1e6f4bba",
+    "url": "/static/css/main.79145f9e.chunk.css"
+  },
+  {
+    "revision": "5129ac3e15ad28217e6a",
+    "url": "/static/js/2.81d72d8c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.81d72d8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3c34370f12d1e6f4bba",
+    "url": "/static/js/main.7e483393.chunk.js"
+  },
+  {
+    "revision": "a108040d8bdcaa9e068a",
+    "url": "/static/js/runtime-main.434ea3e7.js"
+  }
+]);
